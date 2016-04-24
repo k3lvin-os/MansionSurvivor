8727f88f770278a0a8c26dafa4e4b6fded58ff04
kelvingamedev <kelvin.gamedeveloper@gmail.com>
2016-04-24 01:19:37 -0300

    Implementação da chave da Jaula dos Animais por meio de um modelo tercerizado

--- src/mygame/appstates/CameraAppState.java
+++ src/mygame/appstates/CameraAppState.java
@@ -21,7 +21,7 @@ import mygame.javaclasses.Constants;
  */
 public class CameraAppState extends AbstractAppState {
 
-    public static final boolean FLY_CAM_ENABLE = true;
+    public static final boolean FLY_CAM_ENABLE = false;
     private Camera cam;
     private FlyByCamera flyCam;
     private Spatial target;
--- src/mygame/appstates/rooms/OfficeRoom.java
+++ src/mygame/appstates/rooms/OfficeRoom.java
@@ -7,6 +7,7 @@ package mygame.appstates.rooms;
 import mygame.appstates.util.RoomAppState;
 import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import mygame.controls.DoorControl;
@@ -28,9 +29,12 @@ public class OfficeRoom extends RoomAppState {
     public static final float DEFAULT_SIZE = 18f;
     public static final Vector3f DEFAULT_POSITION = CorridorRoom.DEFAULT_LOCATION
             .add(new Vector3f(-DEFAULT_WIDTH, 0f, 0f));
-    public static final Vector3f CORRIDOR_DOOR_POS =  CorridorRoom.OFFICE_DOOR_POS
-            .add( DoorControl.WALL_DISTANCE * -2f,0f,0f);
+    public static final Vector3f CORRIDOR_DOOR_POS = CorridorRoom.OFFICE_DOOR_POS
+            .add(DoorControl.WALL_DISTANCE * -2f, 0f, 0f);
     protected Door corridorDoor;
+    protected Geometry deskWithKey;
+    protected Geometry cagesKey;
+
     public OfficeRoom() {
         super(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_SIZE, DEFAULT_POSITION);
     }
@@ -38,31 +42,38 @@ public class OfficeRoom extends RoomAppState {
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
-        
+
         // Corridor Door
-        DoorOrientation corridorDoorOrientation = new DoorOrientation
-                (RayCastFace.NegativeAxis, Direction.Vertical);
-        this.corridorDoor = new Door(constructionAssets, CORRIDOR_DOOR_POS, 
+        DoorOrientation corridorDoorOrientation = new DoorOrientation(RayCastFace.NegativeAxis, Direction.Vertical);
+        this.corridorDoor = new Door(constructionAssets, CORRIDOR_DOOR_POS,
                 corridorDoorOrientation.getDoorDirection(), nodes.getDoorsNode());
         Geometry corridorDoorGeometry = this.corridorDoor.getPrototypeGeometry().getGeometry();
-        DoorControl corridorDoorControl = new DoorControl(corridorDoorGeometry, 
-                Doors.OFFICE_TO_CORRIDOR, Doors.CORRIDOR_TO_OFFICE, this, 
+        DoorControl corridorDoorControl = new DoorControl(corridorDoorGeometry,
+                Doors.OFFICE_TO_CORRIDOR, Doors.CORRIDOR_TO_OFFICE, this,
                 corridorDoorOrientation, nodes, inputApp);
         corridorDoorGeometry.addControl(corridorDoorControl);
-        
+
+        // Cages Key
+        this.cagesKey = (Geometry) assetManager.loadModel("Models/key/key.j3o");
+        this.cagesKey.setLocalTranslation(-10f,0f,-22f);
+        this.cagesKey.scale(0.125f);
+        this.cagesKey.rotate(FastMath.DEG_TO_RAD * 90F, 0F, 0F);
+
         setEnabled(false);
 
     }
-    
-   @Override
-   public void OnEnabled(){
-       super.OnEnabled();
-       corridorDoor.setEnabled(true);
-   }
-   
-   @Override
-   public void OnDisabled(){
-       super.OnDisabled();
-       corridorDoor.setEnabled(false);
-   }
+
+    @Override
+    public void OnEnabled() {
+        super.OnEnabled();
+        corridorDoor.setEnabled(true);
+        nodes.getRootNode().attachChild(cagesKey);
+    }
+
+    @Override
+    public void OnDisabled() {
+        super.OnDisabled();
+        corridorDoor.setEnabled(false);
+        nodes.getRootNode().detachChild(cagesKey);
+    }
 }
