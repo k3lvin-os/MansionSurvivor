8b23f23ffdab1cb03b24ba5907f5fe63951e438d
kelvingamedev <kelvin.gamedeveloper@gmail.com>
2016-04-24 10:29:18 -0300

    Criação da classe CollisionControl sem seus atributos definidos ainda

---
+++ src/mygame/controls/CollisionControl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.controls;
+
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.control.AbstractControl;
+
+/**
+ *
+ * @author GAMEOVER
+ */
+public class CollisionControl extends AbstractControl {
+
+    @Override
+    protected void controlUpdate(float tpf) {
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+
+    }
+    
+}
